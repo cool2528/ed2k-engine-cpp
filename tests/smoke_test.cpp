ca1eb8625ff7c350d803de29186c0dcae01dc9f1
cool2528 <252858527@qq.com>
2026-07-22 21:42:18 +0800

    release: v2.6.2 - download data integrity + safety

--- tests/smoke_test.cpp
+++ tests/smoke_test.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 #include "ed2k/version.hpp"
 TEST(Smoke, VersionNonEmpty) { EXPECT_FALSE(ed2k::version().empty()); }
-TEST(Smoke, VersionMatchesRelease) { EXPECT_EQ(ed2k::version(), "2.6.1"); }
+TEST(Smoke, VersionMatchesRelease) { EXPECT_EQ(ed2k::version(), "2.6.2"); }
 TEST(Smoke, CompileTimeVersionConstants) {
   static_assert(ed2k::version_major == 2);
   static_assert(ed2k::version_minor == 6);
-  static_assert(ed2k::version_patch == 1);
-  static_assert(ED2K_VERSION_AT_LEAST(2, 6, 1));
+  static_assert(ed2k::version_patch == 2);
+  static_assert(ED2K_VERSION_AT_LEAST(2, 6, 2));
   static_assert(ED2K_VERSION_AT_LEAST(2, 1, 0));
-  static_assert(!ED2K_VERSION_AT_LEAST(2, 6, 2));
+  static_assert(!ED2K_VERSION_AT_LEAST(2, 6, 3));
   static_assert(!ED2K_VERSION_AT_LEAST(3, 0, 0));
 }
