f0531eac69a3f2f978a8873c37d0aa632ab23f11
cool2528 <252858527@qq.com>
2026-07-23 18:04:57 +0800

    release: v2.7.3 - macOS CI coverage + version assertion fix
    Also restores the smoke test version assertions that v2.7.1/v2.7.2
    forgot to bump, which broke CI on those tags.

--- tests/smoke_test.cpp
+++ tests/smoke_test.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 #include "ed2k/version.hpp"
 TEST(Smoke, VersionNonEmpty) { EXPECT_FALSE(ed2k::version().empty()); }
-TEST(Smoke, VersionMatchesRelease) { EXPECT_EQ(ed2k::version(), "2.7.0"); }
+TEST(Smoke, VersionMatchesRelease) { EXPECT_EQ(ed2k::version(), "2.7.3"); }
 TEST(Smoke, CompileTimeVersionConstants) {
   static_assert(ed2k::version_major == 2);
   static_assert(ed2k::version_minor == 7);
-  static_assert(ed2k::version_patch == 0);
-  static_assert(ED2K_VERSION_AT_LEAST(2, 7, 0));
+  static_assert(ed2k::version_patch == 3);
+  static_assert(ED2K_VERSION_AT_LEAST(2, 7, 3));
   static_assert(ED2K_VERSION_AT_LEAST(2, 1, 0));
-  static_assert(!ED2K_VERSION_AT_LEAST(2, 7, 1));
+  static_assert(!ED2K_VERSION_AT_LEAST(2, 7, 4));
   static_assert(!ED2K_VERSION_AT_LEAST(3, 0, 0));
 }
